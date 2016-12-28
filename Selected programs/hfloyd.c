b3e340c721ebd502a9848d5aba96e06c56f337d2
Utsav Das <das.utsav1995@gmail.com>
2016-12-29 01:02:31 +0530

    Add files via upload

---
+++ Selected programs/hfloyd.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include<limits.h>
+#define INFINITY 999
+int i,j,k;
+void floyd(int cost[401][401],int n)
+{
+	for(k=1;k<=n;k++)
+	{
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n;j++)
+			{	
+				
+				if(cost[i][j]>cost[i][k]+cost[k][j])
+				{
+					cost[i][j]=cost[i][k]+cost[k][j];
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	int n,e,a,b,c,no,x,y,cost[401][401];
+	scanf("%d %d",&n,&e);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			cost[i][j]=INFINITY;
+		}
+	}
+	for(i=1;i<=e;i++)
+	{
+		scanf("%d %d %d",&a,&b,&c);
+		cost[a][b]=c;
+	}
+	floyd(cost,n);
+	scanf("%d",&no);
+	for(i=1;i<=no;i++)
+	{
+		scanf("%d %d",&x,&y);
+		if(x==y)
+		{
+                   printf("0\n");
+		   continue;
+			
+		}
+	        else if(cost[x][y] == INFINITY) 
+	        {
+			printf("-1\n");
+			continue;
+		
+		}
+		else
+		printf("%d\n", cost[x][y]);
+	}
+	return 0;
+}
+
