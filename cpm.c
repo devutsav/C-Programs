29ac6c849f92c16915a0a060013a0da47fdb184d
Utsav Das <das.utsav1995@gmail.com>
2016-12-30 02:42:48 +0530

    Update cpm.c

--- cpm.c
+++ cpm.c
@@ -1,117 +1,115 @@
 #include<stdio.h>
-#include<limits.h>
 #define INFINITY 999
 
 int main()
 {
-	int i,j,k,a[10][10],e[10],l[10],count,count1,f[10],m,p,w,n,t;
+	int i,j,k,a[10][10],e[10],l[10],count,count1,arr[10],m,p,w,edge,n,t,t1;
 	printf("Enter the no of nodes\n");
 	scanf("%d",&n);
+	printf("Enter the no of edges\n");
+	scanf("%d",&edge);
 	printf("Enter the vert1 vert2 and edge wt\n");
-	for(i=0;i<n;i++)
-	
-		for(j=0;j<n;j++)
-			a[i][j]=0;
-			while(1)
-			{
-				scanf("%d%d%d",&j,&k,&w);
-				if(j==-1)
-				break;
-					a[j][k]=w;
-				
-			}
-	
-	e[0]=0;
-	for(k=1;k<=n;k++)
+        for(i=0;i<n;i++)
 	{
-		count=0;	
+		for(j=0;j<n;j++)
+		{
+			a[i][j]=0;
+		}
+	}
+	
+	for(i=0;i<edge;i++)
+	{
+		scanf("%d%d%d",&j,&k,&w);
+		a[j][k]=w;
+	}
+	e[0]=0;
+	for(k=0;k<n;k++)
+	{
+		count=0;
 		for(i=0;i<n;i++)
 		{
-			if(a[i][k] != 0)
+			if(a[i][k]!=0)
 			{
 				count++;
 				m=i;
 			}
 		}
-		
 		if(count==1)
 		{
 			e[k]=e[m]+a[m][k];
 		}
 		else
-		{
+		{	
 			e[k]=0;	
-				for(i=0;i<n;i++)
+			for(i=0;i<n;i++)
+			{
+				if(a[i][k]!=0)
 				{
-					if(a[i][k] != 0)
+					t=e[i]+a[i][k];
+					if(t>e[k])
 					{
-						t=e[i]+a[i][k];
-						if(t>e[k])
-						{
-							e[k]=t;
-						}
+						e[k]=t;
 					}
 				}
+			}
 		}
-	}
-	//t=0;
+	}				
 	printf("The forward sums are\n");
 	for(i=0;i<n;i++)
 	{
 		printf("%d ",e[i]);
 	}
-
+        int m1;
 	l[n-1]=e[n-1];
 	for(k=n-2;k>=0;k--)
 	{
 		count1=0;
-		for(j=0;j<n;j++)
+		for(i=0;i<n;i++)
 		{
-			if(a[k][j]!=0)
+			if(a[k][i]!=0)
 			{
 				count1++;
-				p=j;
+				m1=i;
 			}
 		}
-		if(count1==1)
+		if(count==1)
 		{
-			l[k]=l[p]-a[k][p];
+			l[k]=l[m1]-a[k][m1];
 		}
 		else
-		{
-			l[k]=INFINITY;
-			for(j=0;j<n;j++)
+		{	
+			l[k]=INFINITY;	
+			for(i=0;i<n;i++)
 			{
-				if(a[k][j] != 0)
+				if(a[k][i]!=0)
 				{
-					t=l[j]-a[k][j];
-					if(t<l[k])
+					t1=l[i]-a[k][i];
+					if(t1<l[k])
 					{
-						l[k]=t;
+						l[k]=t1;
 					}
 				}
 			}
 		}
-	}
-	printf("The backward sums are\n");
+	}				
+	printf("\nThe backward sums are\n");
 	for(i=0;i<n;i++)
 	{
 		printf("%d ",l[i]);
 	}
 	k=0;
-	for(i=1;i<=n;i++)
+	for(i=0;i<n;i++)
 	{
 		if(e[i]==l[i])
-		{
-			f[k++]=i;
+		{	
+			arr[k++]=i;
 		}
 	}
 	printf("\n");
-	printf("The path is : 0 ");
 	for(i=0;i<k;i++)
 	{
-		printf("  -> %d ",f[i]);
-	}
+		printf(" -> %d ",arr[i]);
+	}			
 	printf("\n");
 	return 0;
-}
+}			
