f3f6fc7cd175f10b5a5e95e83a5afa93c5da36a7
Utsav Das <das.utsav1995@gmail.com>
2016-12-30 16:09:18 +0530

    Update floyds.c

--- floyds.c
+++ floyds.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+/*#include<stdio.h>
 #define INFINITY 999
 int i,j,k;
 int min(int a,int b)
@@ -69,6 +69,73 @@ int main()
 		printf("\n");
 	}
 	printf("\n");
+	return 0;
+}
+*/
+#include <stdio.h>
+#include<limits.h>
+#define INFINITY 999
+int i,j,k;
+void floyd(int cost[401][401],int n,int p[10][10])
+{
+	for(k=1;k<=n;k++)
+	{
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=n;j++)
+			{	
+				
+				if(cost[i][j]>cost[i][k]+cost[k][j])
+				{
+					cost[i][j]=cost[i][k]+cost[k][j];
+					p[i][j]=k;
+				}
+			}
+		}
+	}
+}
+int main()
+{
+	int n,e,a,b,c,x,y,cost[401][401],p[10][10];
+	scanf("%d %d",&n,&e);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			cost[i][j]=INFINITY;
+			p[i][j]=0;
+		}
+	}
+	for(i=1;i<=e;i++)
+	{
+		scanf("%d %d %d",&a,&b,&c);
+		cost[a][b]=c;
+	}
+	printf("The cost matrix is\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d \t",cost[i][j]);
+		}
+	}
+	floyd(cost,n,p);
+	printf("The reduced cost matrix is\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d \t",cost[i][j]);
+		}
+	}
+	printf("The path matrix is\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			printf("%d \t",p[i][j]);
+		}
+	}
 	return 0;
 }
 
