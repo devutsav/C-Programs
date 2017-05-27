fa9019898c8c5846f2e1a4da37c5e3430a1dc40f
Utsav Das <das.utsav1995@gmail.com>
2017-05-27 12:52:14 +0530

    Basic String operations

---
+++ java_progs/str1.java
@@ -0,0 +1,42 @@
+import java.util.*;
+public class str1
+{
+
+     public static void main(String []args)
+     {
+        System.out.println("Enter the sentence");
+        Scanner in = new Scanner(System.in);
+        String l=in.nextLine();
+        System.out.println("Enter the word");
+        String ll = in.next();
+        l = l+" ";
+        String temp="";
+        String newstr = "";
+        for(int i=0;i<l.length();i++)
+        {
+            char x=l.charAt(i);
+	    if(x!=' ')
+	    {
+            	temp = temp + x;
+	    }            
+	    else
+            {
+		if(ll.equals(temp))
+            	{
+                    //newstr = newstr;
+            	}
+		else
+		{
+			newstr = newstr + temp;
+			newstr = newstr + " ";
+		}
+		temp = "";
+	    	
+	    }
+            
+        }
+         System.out.println(newstr);
+     }
+    
+}
+
