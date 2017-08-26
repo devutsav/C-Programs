c3e8c81414467f28af6f3992e5e22e3a258cee7c
Utsav Das <das.utsav1995@gmail.com>
2017-08-27 00:29:34 +0530

    Create k_ary_tree.c

---
+++ Selected programs/k_ary_tree.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include<math.h>
+
+int nodes(int k,int m)
+{
+    long long int under_mod=1000000007;
+    long long int ans=1;
+    for(int i=1;i<=m;i++)
+    {
+      ans=k*ans;
+      ans=ans % under_mod;
+    }
+    return ans;
+}
+int main() {
+	int t,k,m;
+	scanf("%d",&t);
+	while(t--)
+	{
+	    scanf("%d %d",&k,&m);
+	    int a=nodes(k,m);
+	    printf("%d\n",a);
+	}
+	
+
+	return 0;
+}
