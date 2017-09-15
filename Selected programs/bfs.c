3bf6c4827bc31bf3343630893bbab7fbaba45407
Utsav Das <das.utsav1995@gmail.com>
2017-09-15 12:46:08 +0530

    Create bfs.c
    BFS using queue

---
+++ Selected programs/bfs.c
@@ -0,0 +1,39 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int cost[10][10],i,j,k,n;
+int q[10],front,rare,v,visited[10];
+ 
+int main()
+{
+	int m;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	for(k=1;k<=m;k++)
+	{
+		scanf("%d %d",&i,&j);
+		cost[i][j]=1;
+	}
+	scanf("%d",&v);
+	printf("%d ",v);
+	for(i=1;i<=n;i++)
+	{
+		visited[i]=0;
+	}
+	visited[v]=1;
+	k=1;
+	while(k<n)
+	{
+		for(j=1;j<=n;j++)
+			if(cost[v][j]!=0 && visited[j]!=1)
+			{
+				visited[j]=1;
+				q[rare++]=j;
+			}	
+			v=q[front++];
+			printf("%d ",v);
+			k++;
+	}
+	return 0;
+}
