b697ff8cf6bb6f57358a734013ce2e33ecc934ee
Utsav Das <das.utsav1995@gmail.com>
2017-09-18 22:13:39 +0530

    Length of longest common subsequence

---
+++ DP/long_com_subsequence.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+#define max(a,b) a>b?a:b
+int lcs(char str1[],char str2[],int len1, int len2)
+{
+
+	if(len1==0 || len2==0) return 0;
+
+	if(str1[len1-1]==str2[len2-1])
+	{
+		return 1+lcs(str1,str2,len1-1,len2-1);	
+	}
+	else 
+		return 1+max(lcs(str1,str2,len1-1,len2),lcs(str1,str2,len1,len2-1));
+}
+
+int main()
+{
+
+	int i,j,k,n;
+	char str1[]="Utsav";
+	char str2[]="z";
+
+	int len1=strlen(str1);
+	int len2=strlen(str2);
+
+	printf("%d",lcs(str1,str2,len1,len2));
+	return 0;
+}
